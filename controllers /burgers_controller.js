a3e983a5848e1d311f8c3e597286d2347f0e94c0
Joan Marie Verba <verba001@umn.edu>
2018-05-22 13:34:36 -0500

    first commit

---
+++ controllers /burgers_controller.js
@@ -0,0 +1,40 @@
+let express = require("express");
+
+let router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+let burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        let allBurgers = {
+            burgers: data
+        };
+        console.log("index", allBurgers);
+        res.render("index", allBurgers);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    console.log("req.body ", req.body.burger_name, req.body.devoured);
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, 0], function (result) {
+            // Send back the ID of the new burger
+            res.json({ id: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    //let burger_id = "id = " + req.params.id;
+    let burger_id = req.params.id;
+    console.log("burger_id", burger_id);
+
+    burger.updateOne([{ devoured: true }, {id: burger_id }], function (result) {
+        res.send("updated");
+    });
+});
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- Insert a set of records.
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('double cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+
